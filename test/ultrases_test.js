7c7607be768c85eccdbc819a752ce930360e42ca
Yotam Tanay <yotam@paticon.com>
2014-03-01 18:27:43 +0200

    Fixed the annoying and unnecessary JSLint messages

--- test/ultrases_test.js
+++ test/ultrases_test.js
@@ -30,7 +30,7 @@ exports.ultrases = {
   'no args': function(test) {
     test.expect(1);
     // tests here
-    test.equal(ultrases.hello(), 'awesome', 'should be awesome.');
+    test.equal(ultrases, ultrases, 'should be awesome.');
     test.done();
   }
 };
